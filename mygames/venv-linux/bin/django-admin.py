661244efae2d0df21a7129695de407365f882b51
antonio02 <antonio.filho.01@hotmail.com>
2018-12-10 22:13:48 -0300

    adicionando a venv do linux

---
+++ mygames/venv-linux/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/antonio/Área de Trabalho/PI-2018.2/mygames/venv-linux/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

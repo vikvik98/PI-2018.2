5e58c10d459b9a3e628a34e8ac11a3ceb264514e
vikvik98 <vinicius.c.mascarenhas@hotmail.com>
2018-12-10 22:05:21 -0300

    Tirando o avaliador

--- mygames/games/models.py
+++ mygames/games/models.py
@@ -11,18 +11,10 @@ class Publicadora(models.Model):
 
 
 
-class Avaliadora(models.Model):
-    nome = models.CharField(max_length=100)
-
-
-
-
 class Jogo(models.Model):
     nome = models.CharField(max_length=100)
     desenvolvedoras = models.ManyToManyField(Desenvolvedora, through='Desenvolvimento_jogo')
     publicadoras = models.ManyToManyField(Publicadora, through='Publicacao_jogo')
-    avaliacao = models.ManyToManyField(Avaliadora, through='Avaliacao_jogo')
-
 
 
 class Desenvolvimento_jogo(models.Model):
@@ -37,9 +29,3 @@ class Publicacao_jogo(models.Model):
     publicadora = models.ForeignKey(Publicadora, on_delete=models.CASCADE)
     jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
     data_publicacao = models.DateField()
-
-
-class Avaliacao_jogo(models.Model):
-    avaliadora = models.ForeignKey(Avaliadora, on_delete=models.CASCADE)
-    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    nota = models.FloatField()

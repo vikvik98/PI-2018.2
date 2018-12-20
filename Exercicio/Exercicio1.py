bffbbc7a13e304d4b5d7a64c3b0ee83686a20516
vikvik98 <vinicius.c.mascarenhas@hotmail.com>
2018-12-20 04:01:48 -0300

    Exercicio

---
+++ Exercicio/Exercicio1.py
@@ -0,0 +1,32 @@
+import requests
+import wikipedia
+
+
+#Primeira questão
+
+#API 1
+
+# Api para procurar coisas no wikipedia
+
+# d = str(input('Digite sua busca: '))
+# l = wikipedia.search(d)
+# l1 = wikipedia.summary(l[0], sentences=1)
+# print(l1)
+
+#API 2
+
+#Api que procura por musicas,albuns ou artistas
+
+
+# api_key = '2573b6924d7f594eb61735a7dde4bb40'
+# url = 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=shape of you&api_key={0}&format=json'.format(api_key)
+#
+# response = requests.get(url).json()
+#
+# print(response)
+
+
+#API 3
+
+
+

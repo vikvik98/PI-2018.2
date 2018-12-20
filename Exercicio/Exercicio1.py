a61bf33a93b6e0a2d1dfbc31515c2394cd7c38bf
Vinicius Tomaz <32774379+vikvik98@users.noreply.github.com>
2018-12-20 15:12:01 -0300

    Update Exercicio1.py

--- Exercicio/Exercicio1.py
+++ Exercicio/Exercicio1.py
@@ -8,34 +8,34 @@ import wikipedia
 
 # Api para procurar coisas no wikipedia
 
-# d = str(input('Digite sua busca: '))
-# l = wikipedia.search(d)
-# l1 = wikipedia.summary(l[0], sentences=1)
-# print(l1)
+d = str(input('Digite sua busca: '))
+l = wikipedia.search(d)
+l1 = wikipedia.summary(l[0], sentences=1)
+print(l1)
 
 #API 2
 
 #Api que procura por musicas,albuns ou artistas
 
 
-# api_key = '2573b6924d7f594eb61735a7dde4bb40'
-# url = 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=Alivio imediato&api_key={0}&format=json'.format(api_key)
-#
-# response = requests.get(url).json()
-#
-# print(response)
+api_key = '2573b6924d7f594eb61735a7dde4bb40'
+url = 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=Alivio imediato&api_key={0}&format=json'.format(api_key)
+
+response = requests.get(url).json()
+
+print(response)
 
 
 #API 3
 
 # Api do firebase usando um post
 
-# url = 'https://projeto-scrum.firebaseio.com/message_list.json'
-# dadosJson  = {"user_id" : "jack", "text" : "Ahoy!"}
-#
-# response = requests.post(url,json=dadosJson)
-#
-# print(response)
+url = 'https://projeto-scrum.firebaseio.com/message_list.json'
+dadosJson  = {"user_id" : "jack", "text" : "Ahoy!"}
+
+response = requests.post(url,json=dadosJson)
+
+print(response)
 
 
 
